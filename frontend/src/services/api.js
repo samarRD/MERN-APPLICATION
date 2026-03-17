2b30fbf749e8f62b570bc8d0437a097ea4653137
samarRD <samarromdhani32@gmail.com>
2026-03-17 12:44:31 +0100

    update api backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-application-1-ajm7.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
